5ef9d62c83dc3c069311f451d4fa1852bb4853ce
Kathryn Simone <kathrynsimone95@gmail.com>
2019-07-19 11:33:28 -0700

    Express API & MongoDB

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// DB CONFIG
+const db = require('./config/keys').mongoURI;
+
+// CONNECT TO MONGO
+mongoose
+.connect(db, {useNewUrlParser: true})
+.then(() => console.log("MongoDB connected!"))
+.catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server started on port' + port));
